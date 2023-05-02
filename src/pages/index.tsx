edee558160f4dd4bb926c5bc4e5cedfc2a1c6c5e
pione3r <dev2diver@gmail.com>
2023-05-02 15:23:10 +0900

    chore: storybook init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { DragAndDrop } from "@/components";
 import Head from "next/head";
 
 export default function Home() {
@@ -10,9 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <DragAndDrop />
-      </main>
+      <main></main>
     </>
   );
 }
