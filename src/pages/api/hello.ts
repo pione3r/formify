ffb4f8a56ca39b9d34185e0cd99e4e233990147f
pione3r <dev2diver@gmail.com>
2023-05-04 20:02:14 +0900

    chore: 파이어베이스 모듈 설치, db 연결 테스트

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,43 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { collection, getDocs } from "@firebase/firestore";
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 type Data = {
-  name: string
-}
+  name: string;
+  data: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  let id = "";
+  let data = "";
+  const formCollection = collection(db, "form");
+
+  const query = await getDocs(formCollection);
+
+  query.forEach((doc) => {
+    id = doc.id;
+    data = doc.data().askTitle;
+  });
+
+  res.status(200).json({ name: "John Doe", data: data });
 }
