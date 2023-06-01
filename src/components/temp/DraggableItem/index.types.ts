e1907b6d8bfb2b5f3c2f8d5ccc09dff1b90f2d84
pione3r <dev2diver@gmail.com>
2023-06-01 16:28:03 +0900

    feat: 리뉴얼 이전 코드 이동

--- src/components/temp/DraggableItem/index.types.ts
+++
@@ -1,6 +0,0 @@
-import { HTMLAttributes, ReactNode } from "react";
-
-export interface DraggableItemProps extends HTMLAttributes<HTMLDivElement> {
-  children: ReactNode;
-  요소삭제?: (index: number) => void;
-}
